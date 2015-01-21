76d084d0826d6fae5cee50b75e9bc44289d597f0
Brandon Hays <brandon.hays@gmail.com>
2015-01-21 11:14:00 -0600

    expose more typeahead API & test it

--- app/components/twitter-typeahead.js
+++ app/components/twitter-typeahead.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  highlight: false,
+  hint: true,
+  "min-length": 0,
+  minLength: Ember.computed.readOnly("min-length"),
+
   initializeTypeahead: Ember.observer(function() {
     var source = this.get('source');
-    this.$('input.twitter-typeahead-input').typeahead({}, {
+    this.$('input.twitter-typeahead-input').typeahead(this.getProperties('highlight', 'hint', 'minLength'), {
+      displayKey: this.get('displayKey') || function(object) { return object.toString();},
       source: source
     });
   }).on('didInsertElement')
--- tests/unit/components/twitter-typeahead-test.js
+++ tests/unit/components/twitter-typeahead-test.js
@@ -4,10 +4,16 @@ import {
   moduleForComponent,
   test
 } from 'ember-qunit';
+import Ember from 'ember';
+import startApp from "../../helpers/start-app";
+
+var App;
 
 moduleForComponent('twitter-typeahead', 'TwitterTypeaheadComponent', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
+  setup: function() { App = startApp(); },
+  teardown: function() { Ember.run(App, App.destroy); }
 });
 
 var spiedSourceFunc = sinon.spy(function(query, callback) {
@@ -25,4 +31,20 @@ test('it renders', function() {
   // appends the component to the page
   this.append();
   equal(component._state, 'inDOM');
+});
+
+test('entering in keys', function() {
+  expect(3);
+  var component = this.subject({source: spiedSourceFunc});
+  this.append();
+  andThen(function() {
+    var typeahead = $(".tt-input");
+    typeahead.val("te");
+    typeahead.trigger("input");
+  });
+  andThen(function() {
+    equal(component.$(".tt-suggestion").length, 2);
+    equal(component.$(".tt-suggestion:first").text().trim(), "te");
+    equal(component.$(".tt-suggestion:last").text().trim(), "TE");
+  });
 });

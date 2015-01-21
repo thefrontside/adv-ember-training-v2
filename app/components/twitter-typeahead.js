0a8efdfc1fb35eb6dfcbbf87975486ec78b895b1
Brandon Hays <brandon.hays@gmail.com>
2015-01-21 09:48:20 -0600

    initialize typeahead on insertElement

--- app/components/twitter-typeahead.js
+++ app/components/twitter-typeahead.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  initializeTypeahead: Ember.observer(function() {
+    this.$('input.twitter-typeahead-input').typeahead({}, {});
+  }).on('didInsertElement')
 });

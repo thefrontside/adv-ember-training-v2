9500062eaa6f883e8a52a09b2a3bd7259fe7b55b
Brandon Hays <brandon.hays@gmail.com>
2015-01-22 10:01:33 -0600

    prevent non-numeric entry

--- app/components/cc-input.js
+++ app/components/cc-input.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.TextField.extend({
-
+  eventManager: {
+    keyPress: function(e) {
+      if (!String.fromCharCode(e.charCode).match(/\d/)) {
+        e.preventDefault();
+      }
+    }
+  }
 });

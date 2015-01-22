b3d20d3e42f4c6985bea31b4dd1574e209822a68
Brandon Hays <brandon.hays@gmail.com>
2015-01-22 11:30:34 -0600

    wire component to parser

--- app/components/cc-input.js
+++ app/components/cc-input.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
+import Parser from '../utils/card-parser';
 
 export default Ember.TextField.extend({
+  parser: Ember.computed(function() {
+    return Parser.create({
+      inputBinding: Ember.Binding.oneWay("component.value"),
+      component: this
+    });
+  }),
+
+  valueBinding: Ember.Binding.oneWay("parser.formattedOutput"),
+
+  setNumber: Ember.observer("parser.strippedInput", function() {
+    this.set("number", this.get("parser.strippedInput"));
+  }),
+
   eventManager: {
     keyPress: function(e) {
       if (!String.fromCharCode(e.charCode).match(/\d/)) {

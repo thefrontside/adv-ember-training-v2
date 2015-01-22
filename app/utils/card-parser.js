3cc1c8850411829c449d965c41a0d4fd50e517c7
Brandon Hays <brandon.hays@gmail.com>
2015-01-22 11:14:15 -0600

    all cards formatting

--- app/utils/card-parser.js
+++ app/utils/card-parser.js
@@ -1,17 +1,21 @@
 import Ember from 'ember';
 
 var VISA_REGEX = /^4/;
-//var MASTERCARD_REGEX = /^5[1-5]/;
+var MASTERCARD_REGEX = /^5[1-5]/;
 var AMEX_REGEX = /^3[47]/;
-//var DISCOVER_REGEX = /^6(?:011|5)/;
-//var DINERS_CLUB_REGEX = /^3(?:0[0-5]|[68])/;
-//var JCB_REGEX = /^(?:2131|1800|35)/;
+var DISCOVER_REGEX = /^6(?:011|5)/;
+var DINERS_CLUB_REGEX = /^3(?:0[0-5]|[68])/;
+var JCB_REGEX = /^(?:2131|1800|35)/;
 
 export default Ember.Object.extend({
   type: Ember.computed("input", function() {
     var input = this.get("input") || "";
     if (input.match(VISA_REGEX)) {return "visa";}
+    else if (input.match(MASTERCARD_REGEX)) {return "mastercard";}
     else if (input.match(AMEX_REGEX)) {return "amex";}
+    else if (input.match(DISCOVER_REGEX)) {return "discover";}
+    else if (input.match(DINERS_CLUB_REGEX)) {return "diners";}
+    else if (input.match(JCB_REGEX)) {return "jcb";}
     else {return Ember.undefined;}
   }),
 
--- tests/unit/utils/card-parser-test.js
+++ tests/unit/utils/card-parser-test.js
@@ -23,11 +23,30 @@ test('with an invalid number', function() {
   equal(parser.get("formattedOutput"), "0248 3204 3856 2728");
 });
 
-
 test('with an Amex card number', function() {
   parser.set("input", "37828224631");
   equal(parser.get("formattedOutput"), "3782 822463 1");
 
   parser.set("input", "378282246310005");
   equal(parser.get("formattedOutput"), "3782 822463 10005");
+});
+
+test('with a Mastercard card number', function() {
+  parser.set("input", "5555555555554444");
+  equal(parser.get("formattedOutput"), "5555 5555 5555 4444");
+});
+
+test('with a Discover card number', function() {
+  parser.set("input", "6011111111111117");
+  equal(parser.get("formattedOutput"), "6011 1111 1111 1117");
+});
+
+test('with a Diners Club card number', function() {
+  parser.set("input", "30569309025904");
+  equal(parser.get("formattedOutput"), "3056 9309 0259 04");
+});
+
+test('with a JCB card number', function() {
+  parser.set("input", "3530111333300000");
+  equal(parser.get("formattedOutput"), "3530 1113 3330 0000");
 });

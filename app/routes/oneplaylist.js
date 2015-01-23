da5ae20ae31941581222d2f6ae7d93065db9801a
Brandon Hays <brandon.hays@gmail.com>
2015-01-23 12:10:51 -0600

    fetch user and push into store on oneplaylist root

--- app/routes/oneplaylist.js
+++ app/routes/oneplaylist.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    var store = this.store;
+    return Ember.$.getJSON("/user.json").then(function(response){
+      Ember.run(function() {
+        store.pushPayload("user", response);
+      });
+      return store.find("user", response.user.id);
+    });
+  }
 });

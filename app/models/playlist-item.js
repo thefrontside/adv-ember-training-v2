b8601215f69cc1a7eb01ffc6685709927dfcf016
Brandon Hays <brandon.hays@gmail.com>
2015-01-23 00:47:08 -0600

    playlist, playlist-item, user models

--- app/models/playlist-item.js
+++ app/models/playlist-item.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
-  artist: DS.attr(),
-  amazonUrl: DS.attr()
+  song: DS.belongsTo("song"),
+  playlist: DS.belongsTo("playlist")
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,7 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
-  artist: DS.attr(),
-  amazonUrl: DS.attr()
+  playlist: DS.belongsTo("playlist")
 });

e737df17ef663c90130307926c0195d576c93093
Brandon Hays <brandon.hays@gmail.com>
2015-01-23 14:15:52 -0600

    add playlistItem action throws 404

--- app/routes/oneplaylist/songs.js
+++ app/routes/oneplaylist/songs.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find("song");
+  },
+  actions: {
+    addSongToPlaylist: function(song) {
+      this.store.createRecord('playlist-item', {
+        song: song,
+        playlist: this.modelFor("oneplaylist").get('playlist')
+      }).save();
+    }
   }
 });

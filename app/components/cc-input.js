8ec6b6f1b9f53ba94efca84ba1d934dd289ee1f6
Brandon Hays <brandon.hays@gmail.com>
2015-04-02 16:58:44 -0500

    suppress non-numeric digits for cc input

--- app/components/cc-input.js
+++ app/components/cc-input.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import { isDigit } from 'adv-training-helpers';
 
 export default Ember.TextField.extend({
+
+  eventManager: {
+    keyPress: function(event) {
+      if (!isDigit(event)) {
+        event.preventDefault();
+      }
+    }
+  }
 });

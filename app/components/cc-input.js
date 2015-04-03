813c7c839a4275dd288ed4f77ae5f4610278ccaa
Brandon Hays <brandon.hays@gmail.com>
2015-04-03 00:01:46 -0500

    mix in parser for formatting

--- app/components/cc-input.js
+++ app/components/cc-input.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
-import { isDigit } from 'adv-training-helpers';
+import { isDigit, CardInputParser } from 'adv-training-helpers';
 
-export default Ember.TextField.extend({
-
+export default Ember.TextField.extend(CardInputParser, {
   eventManager: {
     keyPress: function(event) {
       if (!isDigit(event)) {

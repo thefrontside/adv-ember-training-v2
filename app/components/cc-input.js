7a84e18b108ebf34fb2e5818098d876ec679bab7
Brandon Hays <brandon.hays@gmail.com>
2015-04-03 00:09:58 -0500

    add is-valid to cc-input

--- app/components/cc-input.js
+++ app/components/cc-input.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 import { isDigit, CardInputParser } from 'adv-training-helpers';
 
 export default Ember.TextField.extend(CardInputParser, {
+  classNameBindings: ["isValid"],
+  isValid: Ember.computed.reads("parser.isValidCard"),
+
   eventManager: {
     keyPress: function(event) {
       if (!isDigit(event)) {

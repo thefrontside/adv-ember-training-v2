bc6bd86b3a58240bdb2c111137a08707dfb4dce8
Brandon Hays <brandon.hays@gmail.com>
2015-04-03 11:21:41 -0500

    add validation logic and disable invalid form

--- app/utils/form.js
+++ app/utils/form.js
@@ -5,33 +5,33 @@ var Form = Ember.Object.extend({
   type: null,
   name: null,
   exp: null,
-  ccv: null
+  ccv: null,
 
   // EMBER COMPTUTED MACROS!
 
   // Use Ember.computed.gte to compute "name.length" as greater or equal to 3
-  // nameIsValid: <IMPLEMENT ME>,
+  nameIsValid: Ember.computed.gte("name.length", 3),
 
   // Use .test() to test this.get("exp") for a regex match
-  // expIsValid: Ember.computed("exp", function() {
-  //   var expRegex = /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/;
-  //   return expRegex.test(this.get("exp"));
-  // }),
+  expIsValid: Ember.computed("exp", function() {
+    var expRegex = /^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$/;
+    return expRegex.test(this.get("exp"));
+  }),
 
   // Just like expIsValid, test against the regex
-  // ccvIsValid: Ember.computed("ccv", function() {
-  //   var ccvRegex = /\d{3,4}/;
-  //   return <IMPLEMENT ME>
-  // }),
+  ccvIsValid: Ember.computed("ccv", function() {
+    var ccvRegex = /\d{3,4}/;
+    return ccvRegex.test(this.get("ccv"));
+  }),
 
   // Use Ember.computd.bool to cast "number" as true or false
-  // numberIsValid: <IMPLEMENT ME>
+  numberIsValid: Ember.computed.bool("number"),
 
   // Use Ember.computed.and for numberIsValid, nameIsValid, expIsValid, and ccvIsValid
-  // isSubmittable: <IMPLEMENT ME>
+  isSubmittable: Ember.computed.and("numberIsValid", "nameIsValid", "expIsValid", "ccvIsValid"),
 
   // Use Ember.computed.not on "isSubmittable"
-  // isInvalid: <IMPLEMENT ME>
+  isInvalid: Ember.computed.not("isSubmittable")
 });
 
 export default Form;

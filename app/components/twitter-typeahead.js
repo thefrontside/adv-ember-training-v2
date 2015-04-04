5fc4e3d3c3dfe82aaa5663eda835ee017ea2ee80
Brandon Hays <brandon.hays@gmail.com>
2015-04-03 20:47:26 -0500

    change function to filter states

--- app/components/twitter-typeahead.js
+++ app/components/twitter-typeahead.js
@@ -3,9 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   didInsertElement: function() {
     this._super.apply(this, arguments);
-    this.$('input').typeahead({}, {
+    var self = this;
+    this.typeahead = this.$('input').typeahead({}, {
       displayKey: this.get('displayKey'),
       source: this.get('source')
+    });
+    this.typeahead.on('typeahead:selected', function(e, suggestion) {
+      Ember.run(function() {
+        self.sendAction("on-select", suggestion);
+      });
     });
   }
 });

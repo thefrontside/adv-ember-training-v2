2b9e9c4a3cda7fa4f70ee38613321b9864ea0a42
Brandon Hays <brandon.hays@gmail.com>
2015-04-05 04:36:33 -0500

    report analytics on didTransition

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,12 +5,12 @@ export default Ember.Route.extend({
   actions: {
   // After creating analytics, call reportEvent on every transition
 
-  //   didTransition: function() {
-  //     this.analytics.reportEvent({
-  //       type: 'pageView',
-  //       location: window.location.href
-  //     });
-  //   }
+    didTransition: function() {
+      this.analytics.reportEvent({
+        type: 'pageView',
+        location: window.location.href
+      });
+    }
 
   }
 });

0c400cb6210250b26b3cc625a96a22c420866566
Brandon Hays <brandon.hays@gmail.com>
2015-04-05 01:58:04 -0500

    ajax and pushPayload for user data

--- app/routes/oneplaylist.js
+++ app/routes/oneplaylist.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     // This might be a good time to try Ember.$.ajax() and this.store.pushPayload :)
+    var store = this.store;
+    return Ember.$.getJSON("/user.json").then(function(response){
+      Ember.run(function() {
+        store.pushPayload("user", response);
+      });
+      return store.find("user", response.user.id);
+    });
   }
 });

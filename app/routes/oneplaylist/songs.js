abcc48fad75437146f575d0c26292caa79955d88
Brandon Hays <brandon.hays@gmail.com>
2015-04-05 02:37:17 -0500

    implement addSongToPlaylist action

--- app/routes/oneplaylist/songs.js
+++ app/routes/oneplaylist/songs.js
@@ -7,13 +7,10 @@ export default Ember.Route.extend({
   },
   actions: {
     addSongToPlaylist: function(song) {
-      alert("Implement me! Song: " + song.get("name"));
-      // We need to:
-      // 1) create a playlist-item record
-      // 2) set its song to the song argument
-      // 3) set its playlist to the Playlist model from the oneplaylist route
-      // 4) call save() on the new playlistItem
-      // HINT: Use this.modelFor("oneplaylist").get("playlist") to get the playlist
+      this.store.createRecord('playlist-item', {
+        song: song,
+        playlist: this.modelFor("oneplaylist").get('playlist')
+      }).save();
     }
 
     // Next, we need a removePlaylistItem action that just calls destroyRecord()
